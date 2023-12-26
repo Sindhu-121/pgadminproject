a31d8d4a827b369e439e0b68f497f4523b3f788f
sindhu-121 <sindhu.g@egradtutor.in>
2023-12-26 10:20:33 +0530

    happy

--- server/pgadmin.js
+++ server/pgadmin.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3083;
+app.use(express.json());
+app.use(cors());
+const db = require('./pgdb');
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
